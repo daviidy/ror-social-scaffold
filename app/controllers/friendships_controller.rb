f70af1ad7621578525555e2a972470fe4201a02e
daviidy <>
2020-12-18 09:46:43 +0000

    refactor frindships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,16 +1,24 @@
 class FriendshipsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_friendship, only: %i[create]
+
   # see all invitations and friends
   def index; end
 
   # send friend request
   def create
-    @friendship = current_user.friendships.new(user_id: current_user.id, friend_id: params[:user_id])
+    if @friendship.nil?
+      @request = current_user.friendships.new(user_id: current_user.id, friend_id: params[:user_id])
 
-    if @friendship.save
-      flash.notice = 'Request sent!'
-      redirect_back(fallback_location: root_path)
+      if @request.save
+        flash.notice = 'Request sent!'
+        redirect_back(fallback_location: root_path)
+      else
+        render :new
+      end
     else
-      render :new
+      flash.notice = 'There is already a friend request pending for this user!'
+      redirect_back(fallback_location: root_path)
     end
   end
 
@@ -23,9 +31,16 @@ class FriendshipsController < ApplicationController
 
   def destroy
     @user = User.find(params[:user_id])
-    @friendship = current_user.inverse_friendships.where('user_id = ?', @user.id).first
-    @friendship.destroy
+    @request = current_user.inverse_friendships.where('user_id = ?', @user.id).first
+    @request.destroy
     flash.notice = 'Request rejected!'
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_friendship
+    @friendship = Friendship.find_by(user_id: params[:user_id], friend_id: current_user.id) ||
+                  Friendship.find_by(user_id: current_user.id, friend_id: params[:user_id])
   end
 end
